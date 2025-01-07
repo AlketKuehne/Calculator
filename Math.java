7ee0bb1c00593b66d41dcc4d7e2dfeae591b767a
AlketKuehne <alketrrushi0@gmail.com>
2025-01-07 11:52:12 +0000

    Math.java

--- Math.java
+++ Math.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class Fraction {
+public class Math {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-        double numerator, denominator, result;
+        double number1, number2, result;
         
-        System.out.print ("Enter Numerator: ");
-        numerator = scanner.nextInt ();
-        System.out.print ("Enter Denominator: ");
-        denominator = scanner.nextInt ();
+        System.out.print ("Enter Number: ");
+        number1 = scanner.nextInt ();
+        System.out.print ("Enter Number: ");
+        number2 = scanner.nextInt ();
         System.out.println ();
 
-        result = numerator / denominator;
+        result = number1 / number2;
 
-        System.out.println (numerator + "/" + denominator + " = " + result);
+        System.out.println (number1 + "/" + number2 + " = " + result);
     }
 }
