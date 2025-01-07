4d9078a993fd94e3480301ddf41c0f6f495865da
AlketKuehne <alketrrushi0@gmail.com>
2025-01-07 10:42:25 +0000

    Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Fraction {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-        int factor1, factor2, result;
+        double numerator, denominator, result;
         
-        System.out.print ("Enter Factor: ");
-        factor1 = scanner.nextInt ();
-        System.out.print ("Enter Factor: ");
-        factor2 = scanner.nextInt ();
+        System.out.print ("Enter Numerator: ");
+        numerator = scanner.nextInt ();
+        System.out.print ("Enter Denominator: ");
+        denominator = scanner.nextInt ();
         System.out.println ();
 
-        result = factor1 * factor2;
+        result = numerator / denominator;
 
-        System.out.println (factor1 + "*" + factor2 + " = " + result);
+        System.out.println (numerator + "/" + denominator + " = " + result);
     }
 }
